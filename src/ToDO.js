34d5abddaf4d07afee84c81bf786de9703f18ad3
Kalitha <kalithadissanayake44@gmail.com>
2022-02-10 18:28:20 +0530

    function to add new entries

--- src/ToDO.js
+++ src/ToDO.js
@@ -1,57 +1,48 @@
 import React, { useState } from 'react'
 
 function Todo() {
-    const handelOnChane = (e) => {
 
-    }
-    const initialState = [
-        {
-            id: 1,
-            task: 'task 1',
-            isCompleted: 'Not Done'
-        },
-
-        {
-            id: 2,
-            task: 'ttask 2',
-            isCompleted: 'Not Done'
-        }
-    ]
+    const initialTask = ''
 
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log(task)
-      }
+        console.log(todos.length)
+        setTodos([...todos, { id: todos.length, task: task, isCompleted: 'Not Done' }])
+        setTask('')
+    }
 
 
     const taskcomp = (id) => {
         console.log(id)
         let newState = [...todos];
-        newState[id - 1].isCompleted = ' Done';
+        newState[id].isCompleted = ' Done';
         setTodos(newState)
     }
 
 
-    const [todos, setTodos] = useState(initialState)
-    const [task, setTask] = useState()
+
+    const [todos, setTodos] = useState([])
+    const [task, setTask] = useState(initialTask)
     console.log(todos)
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" onChange={(e) =>setTask(e.target.value)} />
+                <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
                 <button >add</button>
             </form>
-            {
-                todos.map(item => (
+            <ul>
+                {
+                    todos.map(item => (
 
 
-                    <li key={item.id}>{item.task} status - {item.isCompleted} <button onClick={() => taskcomp(item.id)}>done</button></li>
+                        <li key={item.id}>{item.task} status - {item.isCompleted} <button onClick={() => taskcomp(item.id)}>done</button></li>
 
 
-                ))}
+                    ))}
 
-
+            </ul>
         </div>
     )
 }
