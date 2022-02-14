cfa158a76b2f35d5b441316593f10776f3cfa1e3
Kalitha <kalithadissanayake44@gmail.com>
2022-02-14 09:17:00 +0530

    completed message updated

--- src/ToDO.js
+++ src/ToDO.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Todo() {
 
     const initialTask = ''
 
+  
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -11,6 +12,9 @@ function Todo() {
         console.log(todos.length)
         setTodos([...todos, { id: todos.length, task: task, isCompleted: 'Not Done' }])
         setTask('')
+        setmessage('Added a new task')
+
+
     }
 
 
@@ -19,25 +23,41 @@ function Todo() {
         let newState = [...todos];
         newState[id].isCompleted = ' Done';
         setTodos(newState)
+        setmessage(`${newState[id].task } Updated`)
     }
 
 
 
     const [todos, setTodos] = useState([])
     const [task, setTask] = useState(initialTask)
+    const [message, setmessage] = useState('')
     console.log(todos)
+
+
+    useEffect(() => {
+        console.log('add')
+       setTimeout(() => {
+            console.log('timer')
+            setmessage('')
+        }, 1000);
+  
+       
+      }, [todos]);
+
+
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <input type="text" value={task} onChange={(e) => setTask(e.target.value)} />
                 <button >add</button>
             </form>
+            <div >{message}</div>
             <ul>
                 {
                     todos.map(item => (
 
 
-                        <li key={item.id}>{item.task} status - {item.isCompleted} <button onClick={() => taskcomp(item.id)}>done</button></li>
+                        <li key={item.id}>{item.task}   ||- status - {item.isCompleted} <button onClick={() => taskcomp(item.id)}>done</button></li>
 
 
                     ))}
